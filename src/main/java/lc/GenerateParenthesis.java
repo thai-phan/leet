071a2af8760bb8ececf9b1d18f18930011df8a58
thai-phan <thaiph@gaion.kr>
2024-08-05 14:40:40 +0900

    add solution generateParenthesis

--- src/main/java/lc/GenerateParenthesis.java
+++ src/main/java/lc/GenerateParenthesis.java
@@ -4,7 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GenerateParenthesis {
-  public List<String> generateParenthesis(int n) {
+  public static List<String> generateParenthesis(int numberParenthesis) {
+    List<String> result = new ArrayList<String>();
+    recurse(result, 0, 0, "", numberParenthesis);
+    return result;
+  }
+
+  public static void recurse(List<String> result, int left, int right, String s, int numberParenthesis) {
+    if (s.length() == numberParenthesis * 2) {
+      result.add(s);
+      return;
+    }
+
+    if (left < numberParenthesis) {
+      recurse(result, left + 1, right, s + "(", numberParenthesis);
+    }
+
+    if (right < left) {
+      recurse(result, left, right + 1, s + ")", numberParenthesis);
+    }
+  }
+  // See above tree diagram with parameters (left, right, s) for better understanding
+
+  public List<String> myGenerateParenthesis(int n) {
     List<String> finalList = new ArrayList<>();
     if (n == 1) {
       finalList.add("()");
@@ -31,6 +53,9 @@ public class GenerateParenthesis {
   public static void main(String[] args) {
     var aa = insertMiddle("aaabbb", "ccc");
     System.out.println(aa);
+
+    var bbb = generateParenthesis(3);
+    System.out.println(bbb);
 
 
   }
