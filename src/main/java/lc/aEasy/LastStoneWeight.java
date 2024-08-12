ccce0647df7b685568cd2314b5b15da57b682848
Thai Phan <thai-phan@github.com>
2024-08-12 22:22:34 +0900

    priority queue

---
+++ src/main/java/lc/aEasy/LastStoneWeight.java
@@ -0,0 +1,41 @@
+package lc.aEasy;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+
+  public static int lastStoneWeight(int[] stones) {
+    Comparator<Integer> comparator = (o1, o2) -> {
+      if (o1 > o2) return 1;
+      return 0;
+    };
+    PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+    for (int stone : stones) {
+      queue.offer(stone);
+    }
+    while (queue.size() > 1) {
+      int last = queue.poll();
+      int last2 = queue.poll();
+      if (last > last2) {
+        queue.offer(last - last2);
+      }
+    }
+
+    if (queue.size() == 1)
+      return queue.poll();
+    else
+      return 0;
+  }
+
+  public static  void main(String[] args) {
+    int[] stones = {2,7,4,1,8,1};
+
+    System.out.println(lastStoneWeight(stones));
+
+
+  }
+
+
+
+}
