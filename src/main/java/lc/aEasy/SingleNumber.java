9b60fd48b0fd42d1134e96be3832fa7438836549
thai-phan <thaiph@gaion.kr>
2024-08-14 11:46:48 +0900

    update single number

--- src/main/java/lc/aEasy/SingleNumber.java
+++ src/main/java/lc/aEasy/SingleNumber.java
@@ -6,19 +6,19 @@ import java.util.Map;
 // 136. Single Number
 public class SingleNumber {
 
-  public int singleNumber(int[] nums) {
-    Map<Integer, Integer> aaa = new HashMap();
+  public static int singleNumber(int[] nums) {
+    int result = 0;
     for (int num : nums) {
-      if (aaa.containsKey(num)) {
-        aaa.put(num, aaa.get(num) + 1);
-      } else {
-        aaa.put(num, 1);
-      }
+      result = result ^ num;
+      System.out.println(result);
     }
-    for (Map.Entry<Integer, Integer> entry : aaa.entrySet()) {
-      if (entry.getValue() == 1) return entry.getKey();
-    }
-    return nums[0];
+    return result;
+  }
+
+  public static void main(String[] args) {
+
+    var aaa = singleNumber(new int[]{2,2,3,3,4,4,1,5});
+//    System.out.println(aaa);
   }
 
 }
