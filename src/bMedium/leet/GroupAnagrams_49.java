d02b0bee614ccfdf130307566a9791a8c86139a0
thaiphan <thaiphan2712@gmail.com>
2025-05-29 20:54:24 -0500

    update structure

---
+++ src/bMedium/leet/GroupAnagrams_49.java
@@ -0,0 +1,26 @@
+package bMedium.leet;
+
+import java.util.*;
+
+public class GroupAnagrams_49 {
+  public static void main(String[] args) {
+    String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+
+    groupAnagrams(strs);
+
+  }
+
+  public static List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> map = new HashMap<String, List<String>>();
+
+    for (String s : strs) {
+      char[] ca = s.toCharArray();
+      Arrays.sort(ca);
+      String key = String.valueOf(ca);
+
+      map.putIfAbsent(key, new ArrayList<>());
+      map.get(key).add(s);
+    }
+    return new ArrayList<List<String>>(map.values());
+  }
+}
